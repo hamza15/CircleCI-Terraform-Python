2ef9b2b138cbb94c45fc4288b006b7c99cb1ff2a
Sardar Hamza Naqi <sardan5@vt.edu>
2020-02-04 00:20:55 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,8 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def greet():
+    greeting = 'Hello World from BenchSci!'
+    return greeting
+
 @app.route('/hello')
 def helloIndex():
-    return 'Hello World from BenchSci!'
+    return greet()
 
 app.run(host='0.0.0.0', port= 8080)
