640e6c34bd03c595eb10fe429949df47b9d4fb8b
Sardar Hamza Naqi <sardan5@vt.edu>
2020-02-04 00:45:54 -0500

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -10,6 +10,11 @@ class TestHelloWorld(unittest.TestCase):
     def test_status_code(self):
         response = self.app.get('/hello')
         self.assertEqual(response.status_code, 200)
+        
+    def test_greeting_message(self):
+        greeting = 'Hello World from BenchSci!'
+        self.assertEqual(hello_world.greet(), greeting)
+        
 
 if __name__ == '__main__':
     unittest.main()
