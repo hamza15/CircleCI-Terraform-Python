1771822709c479b3cfece4d64c4737077510e42e
Sardar Hamza Naqi <sardan5@vt.edu>
2020-02-06 01:49:31 -0500

    Update hello_world.py

--- webserver/hello_world.py
+++ webserver/hello_world.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def helloIndex():
-    return 'Hello World from BenchSci!'
+    return 'Bye World from BenchSci!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port= 80)
